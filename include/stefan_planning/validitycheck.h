b6b1cc7907a4975889a7b017790f46d915801ab3
jiyeongbaek <jy951111@gmail.com>
2020-01-28 14:56:33 +0900

    add iksolver

--- include/stefan_planning/validitycheck.h
+++ include/stefan_planning/validitycheck.h
@@ -33,9 +33,10 @@ public:
     ValidityCheck(ros::NodeHandle nh, const ob::SpaceInformationPtr &si) : ob::StateValidityChecker(si)
     {
         nh.param("urdf_param", urdf_param, std::string("/robot_description"));
+        /* grasping point */
     }
 
-    void ik_solver(Affine3d target_pos)
+    bool ik_solver(Affine3d target_pos) const
     {
         double eps = 1e-5;
         int num_samples = 1000;
@@ -51,14 +52,14 @@ public:
         if (!valid)
         {
             ROS_ERROR("There was no valid KDL chain found");
-            return;
+            return false;
         }
 
         valid = tracik_solver.getKDLLimits(ll, ul);
         if (!valid)
         {
             ROS_ERROR("There were no valid KDL joint limits found");
-            return;
+            return false;
         }
 
         assert(chain.getNrOfJoints() == ll.data.size());
@@ -82,32 +83,51 @@ public:
         for (size_t i = 0; i < 3; i++)
             end_effector_pose.p(i) = target_pos.translation()[i];
 
-        // end_effector_pose.M.Quaternion(target_pos.linear().coeffs()[0], target_pos.linear().coeffs()[1], target_pos.linear().coeffs()[2], target_pos.linear().coeffs()[3]);
-        int rc;
+        Matrix3d Rot_d = target_pos.linear();
+        KDL::Rotation A;
+        A.data[0] = Rot_d(0, 0);
+        A.data[1] = Rot_d(0, 1);
+        A.data[2] = Rot_d(0, 2);
+        A.data[3] = Rot_d(1, 0);
+        A.data[4] = Rot_d(1, 1);
+        A.data[5] = Rot_d(1, 2);
+        A.data[6] = Rot_d(2, 0);
+        A.data[7] = Rot_d(2, 1);
+        A.data[8] = Rot_d(2, 2);
+        end_effector_pose.M = A;
+
+        int rc = -1;
         for (uint i = 0; i < num_samples; i++)
         {
             double elapsed = 0;
             rc = tracik_solver.CartToJnt(nominal, end_effector_pose, result);
+            // std::cout << rc << std::endl;
             if (rc >= 0)
-                break;
-            //   success++;
+            {
+                ROS_INFO("FOUND SOLUTION");
+                return true;
+            }
         }
+        return false;
     }
 
     virtual bool isValid(const ob::State *state) const
     {
-        /* grasping point */
-        Affine3d base_left, base_right, base_object, left_object, right_object;
+        Affine3d base_left, base_right, left_object, right_object;
         Affine3d obj_Lgrasp, obj_Rgrasp;    //defined by object frame CONSTANT
         Affine3d left_Lgrasp, right_Rgrasp; //defined by each robot frame
+        obj_Lgrasp.translation() = Vector3d(-0.414640147082728, 0.298136277133603, 0.0177842219404252);
+        Quaterniond q_Lgrasp(0.0090642, 0.720166, -0.693648, 0.0114454 );
+        obj_Lgrasp.linear() = q_Lgrasp.toRotationMatrix();
 
-        // obj_Lgrasp.translation() = ;
-        // obj_Lgrasp.linear() = ;
-        // obj_Rgrasp.translation() = ;
-        // obj_Rgrasp.linear() = ;
+        obj_Rgrasp.translation() = Vector3d(-0.410945269338053, 0.175340483200395, 0.35246196213878);
+        Quaterniond q_Rgrasp( -0.0630359, 0.717459, -0.690838, -0.0634221);
+        obj_Rgrasp.linear() = q_Rgrasp.toRotationMatrix();
 
-        // base_left.translation() = Vector3d(0. 0.2, 0);
-        // base_right.translation() = Vector3d(0, -0.2, 0);
+        base_left.translation() = Vector3d(0, 0.2, 0.6);
+        base_right.translation() = Vector3d(0, -0.2, 0.6);
+        base_left.linear().setIdentity();
+        base_right.linear().setIdentity();
 
         // cast the abstract state type to the type we expect
         const auto *se3state = state->as<ob::SE3StateSpace::StateType>();
@@ -117,20 +137,18 @@ public:
 
         // extract the second component of the state and cast it to what we expect
         const auto *rot = se3state->as<ob::SO3StateSpace::StateType>(1);
-        // for (int i = 0; i < 3; i++)
-        //     base_object.translation()[i] = pos(i);
-        // base_object.linear() = rot;
-        left_object = base_left.inverse() * base_object;
-        right_object = base_right.inverse() * base_object;
 
-        left_Lgrasp = left_object * obj_Lgrasp;
-        right_Rgrasp = right_object * obj_Rgrasp;
+        Affine3d base_object;
+        base_object.translation() = Vector3d(pos->values[0], pos->values[1], pos->values[2]);
+        base_object.linear() = Quaterniond(rot->w, rot->x, rot->y, rot->z).toRotationMatrix();
 
-        // ik_solver(left_Lgrasp);
-        // ik_solver(right_Rgrasp);
+        left_Lgrasp = base_left.inverse() * base_object * obj_Lgrasp;
+        right_Rgrasp = base_right.inverse() * base_object * obj_Rgrasp;
+
+        return (ik_solver(left_Lgrasp) && ik_solver(right_Rgrasp));
 
         // return a value that is always true but uses the two variables we define, so we avoid compiler warnings
-        return (const void *)rot != (const void *)pos;
+        // return (const void *)rot != (const void *)pos;
     }
 
     std::string urdf_param;
@@ -155,26 +173,31 @@ public:
     void setStartAndGoal()
     {
         ob::ScopedState<> start(space);
-        start[0] = 1.1;
-        start[1] = -0.15;
-        start[2] = 0.601;
-        start[3] = 0.0;
-        start[4] = 0.0;
-        start[5] = 0.0;
-        start[6] = 1.0;
+        auto *se3state = start->as<ob::SE3StateSpace::StateType>();
+        auto *pos = se3state->as<ob::RealVectorStateSpace::StateType>(0);
+        auto *rot = se3state->as<ob::SO3StateSpace::StateType>(1);
+        pos->values[0] = 1.1;
+        pos->values[1] = -0.15;
+        pos->values[2] = 0.601;
+        rot->x = rot->y = rot->z = 0.0;
+        rot->w = 1.0;
         cout << start << endl;
 
         // create a random goal state
         ob::ScopedState<> goal(space);
-        goal[0] = 1.1;
-        goal[1] = 0.15;
-        goal[2] = 0.625;
+        auto *se3state2 = goal->as<ob::SE3StateSpace::StateType>();
+        auto *pos2 = se3state2->as<ob::RealVectorStateSpace::StateType>(0);
+        auto *rot2 = se3state2->as<ob::SO3StateSpace::StateType>(1);
+        pos2->values[0] = 1.1;
+        pos2->values[1] = 0.15;
+        pos2->values[2] = 0.625;
         Eigen::Quaterniond q;
         q = AngleAxisd(deg2rad(85), Vector3d::UnitX()) * AngleAxisd(deg2rad(3), Vector3d::UnitY()) * AngleAxisd(0, Vector3d::UnitZ());
-        goal[3] = q.coeffs()[0];
-        goal[4] = q.coeffs()[1];
-        goal[5] = q.coeffs()[2];
-        goal[6] = q.coeffs()[3];
+        rot2->x = q.coeffs().x();
+        rot2->y = q.coeffs().y();
+        rot2->z = q.coeffs().z();
+        rot2->w = q.coeffs().w();
+
         cout << goal << endl;
         pdef->setStartAndGoalStates(start, goal);
     }
@@ -183,9 +206,12 @@ public:
     {
         // set the bounds for the R^3 part of SE(3)
         ob::RealVectorBounds bounds(3);
-        bounds.setLow(-2);
-        bounds.setHigh(2);
-
+        bounds.setLow(0, 0.5);
+        bounds.setHigh(0, 2.5);
+        bounds.setLow(1, -1.5);
+        bounds.setHigh(1, 1.5);
+        bounds.setLow(2, 0.6);
+        bounds.setHigh(2, 1.6);
         space->setBounds(bounds);
 
         si->setStateValidityChecker(vc);
@@ -208,7 +234,7 @@ public:
         // pdef->print(std::cout);
 
         // attempt to solve the problem within one second of planning time
-        ob::PlannerStatus solved = planner->ob::Planner::solve(1.0);
+        ob::PlannerStatus solved = planner->ob::Planner::solve(600.0);
 
         if (solved)
         {
